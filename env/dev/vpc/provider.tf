b1cb6a41091d82a2d8a44f795d38c4dfe8fadf61
zalak_92 <zalak.dharia@outlook.com>
2026-02-04 12:21:57 +0530

    feat: add s3 backend for VPC dev environment

--- env/dev/vpc/provider.tf
+++ env/dev/vpc/provider.tf
@@ -6,9 +6,15 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
-
 provider "aws" {
   region  = var.region
   profile = var.aws_profile
+}
+backend "s3" {
+    bucket         = "zalak-terraform-state"
+    key            = "aws/dev/vpc/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 }
